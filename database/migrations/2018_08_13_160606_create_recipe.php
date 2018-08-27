4196854b13eb53d8b88fc5b9cda4f99eb9693a1c
Michael Sullivan <msullivan@myriadmobile.com>
2018-08-27 09:09:48 -0500

    Commit changes from this weekend

--- database/migrations/2018_08_13_160606_create_recipe.php
+++ database/migrations/2018_08_13_160606_create_recipe.php
@@ -15,19 +15,17 @@ class CreateRecipe extends Migration
     {
       Schema::create('recipe', function (Blueprint $table) {
         $table->increments('id');
-        $table->unsignedInteger('event_id');
         $table->string('recipe_image');
         $table->date('date');
         $table->string('title');
-        $table->string('creator'); //The design has this as a text field, but it seems like it should just be assigned to the user creating it?
+        $table->string('creator');
         $table->unsignedInteger('category_id');
         $table->string('servings');
-        $table->string('difficulty'); //What do you like to do when there is a defined array of options availible (easy, medium, hard) to pick from
+        $table->string('difficulty'); //What do you like to do when there is a defined array of options available (easy, medium, hard) to pick from
         $table->string('time');
         $table->text('ingredients');
         $table->text('directions');
 
-        $table->foreign('event_id')->references('id')->on('event');
         $table->foreign('category_id')->references('id')->on('category');
 
       });
--- database/migrations/2018_08_24_172240_create_event_category.php
+++ database/migrations/2018_08_24_172240_create_event_category.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategory extends Migration
+class CreateEventCategories extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateCategory extends Migration
      */
     public function up()
     {
-       Schema::create('category', function (Blueprint $table) {
-        $table->increments('id');
-        $table->string('name');
+      Schema::create('event_category', function (Blueprint $table) {
+        $table->unsignedInteger('event_id');
+        $table->unsignedInteger('category_id');
+        $table->unsignedInteger('category_count');
       });
     }
 
@@ -26,6 +27,6 @@ class CreateCategory extends Migration
      */
     public function down()
     {
-      Schema::dropIfExists('category');
+      Schema::dropIfExists('event_category');
     }
 }
